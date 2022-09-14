eb4feecab90675d0ef45103c9cf9694a3c1c0a0e
Renato Adorno <renatowillian.2000@gmail.com>
2022-09-14 13:32:21 -0300

    test: adds test_decorar_relatorio

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,46 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+PRODUCTS = [
+    {
+        "id": "1",
+        "nome_do_produto": "Produto 1",
+        "nome_da_empresa": "Empresa 1",
+        "data_de_fabricacao": "2021-05-01",
+        "data_de_validade": "2023-06-02",
+        "numero_de_serie": "001",
+        "instrucoes_de_armazenamento": "Armazenar em local seguro",
+    },
+    {
+        "id": "2",
+        "nome_do_produto": "Produto 2",
+        "nome_da_empresa": "Empresa 2",
+        "data_de_fabricacao": "2022-06-15",
+        "data_de_validade": "2023-07-15",
+        "numero_de_serie": "002",
+        "instrucoes_de_armazenamento": "Armazenar em local quente",
+    },
+    {
+        "id": "3",
+        "nome_do_produto": "Produto 3",
+        "nome_da_empresa": "Empresa 3",
+        "data_de_fabricacao": "2022-06-15",
+        "data_de_validade": "2023-07-15",
+        "numero_de_serie": "003",
+        "instrucoes_de_armazenamento": "Armazenar em local frio",
+    },
+]
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    report = ColoredReport(SimpleReport)
+
+    send_report = report.generate(PRODUCTS)
+
+    assert "\033[32mData de fabricação mais antiga:\033[0m" in send_report
+    assert "\033[36m2021-05-01\033[0m" in send_report
+    assert "\033[32mData de validade mais próxima:\033[0m" in send_report
+    assert "\033[36m2023-06-02\033[0m" in send_report
+    assert "\033[32mEmpresa com mais produtos:\033[0m" in send_report
+    assert "\033[31mEmpresa 2\033[0m" in send_report
