2150bd6c9b6a21f287f180166784f71daefdd528
Renato Adorno <renatowillian.2000@gmail.com>
2022-09-14 13:23:57 -0300

    test: adds test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Produto 1",
+        "Empresa 1",
+        "2018-06-06",
+        "2025-08-22",
+        "001",
+        "Armazenar em local Seguro",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "Monitor"
+    assert product.nome_da_empresa == "Teclado"
+    assert product.data_de_fabricacao == "2018-06-06"
+    assert product.data_de_validade == "2025-08-22"
+    assert product.numero_de_serie == "001"
+    assert product.instrucoes_de_armazenamento == "Armazenar em local Seguro"
