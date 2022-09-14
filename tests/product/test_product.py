db57c32403c775389129791a82236ee4ca0176e8
Renato Adorno <renatowillian.2000@gmail.com>
2022-09-14 14:11:46 -0300

    feat: erro mock

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -4,17 +4,17 @@ from inventory_report.inventory.product import Product
 def test_cria_produto():
     product = Product(
         1,
-        "Todynho 200ml",
-        "PepsiCo",
-        "2022-06-16",
-        "2023-08-15",
-        "87340918374189237",
-        "Conservar em local seco.",
+        "Produto 1",
+        "Empresa 1",
+        "2018-06-06",
+        "2025-08-22",
+        "001",
+        "Armazenar em local Seguro",
     )
     assert product.id == 1
-    assert product.nome_do_produto == "Todynho 200ml"
-    assert product.nome_da_empresa == "PepsiCo"
-    assert product.data_de_fabricacao == "2022-06-16"
-    assert product.data_de_validade == "2023-08-15"
-    assert product.numero_de_serie == "87340918374189237"
-    assert product.instrucoes_de_armazenamento == "Conservar em local seco."
+    assert product.nome_do_produto == "Produto 1"
+    assert product.nome_da_empresa == "Empresa 1"
+    assert product.data_de_fabricacao == "2018-06-06"
+    assert product.data_de_validade == "2025-08-22"
+    assert product.numero_de_serie == "001"
+    assert product.instrucoes_de_armazenamento == "Armazenar em local Seguro"
